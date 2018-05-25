7c890c3e0afff4472bb320c045a3de8f79c596e3
SteveWalsh1989 <stevewalsh89@gmail.com>
2018-05-25 16:15:03 +0100

    added extra checks to TestnewDeck

--- main/deck_test.go
+++ main/deck_test.go
@@ -16,10 +16,28 @@ func TestNewDeck(t *testing.T){
 
 	d := newDeck()
 
-	if len(d) != 52 { 		// Scenario 1: Error, not correct deck size created
+	if len(d) != 52 { 										  // Scenario 1: Error, not correct deck size created
 
 		t.Error("Expected deck lenght of: 52" +
-			"		 \n Created length of: ", len(d)) // print error
+			"		 \n Created length of: ", len(d)) 		  // print error
+	}
+
+
+	if d[0] != "Ace of Diamonds" {   						  // Scenario 2: Error, first card not correct
+
+
+		t.Error("Expected first card: Ace of Diamonds" +
+			"		 \n First card instead: ", d[0])          // print error
+
+	}
+
+
+	if d[len(d) -1 ] != "Queen of Clubs" { 					  // Scenario 3: Error, last card not correct
+
+
+		t.Error("Expected first card: Queen of Clubs" +
+			"		 \n First card instead: ", d[len(d) -1 ]) // print error
+
 	}
 
 }
